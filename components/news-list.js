e09ffd3fb09abd3fe00d68e946fe1a3b90caa453
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-01-20 23:08:02 +0100

    create archive page and parallel routes

---
+++ components/news-list.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NewsList({ news }) {
+  return (
+    <ul className="news-list">
+      {news.map((news) => {
+        return (
+          <li key={news.id}>
+            <Link href={`/news/${news.slug}`}>
+              <img src={`/images/news/${news.image}`} alt={news.title} />
+              <span>{news.title}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
